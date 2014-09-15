a2d6397020e69e84893e218aca7d84a1664ab4c1
jijojose <jijojose@tataelxsi.co.in>
2014-09-15 14:48:31 +0530

    commit sample

---
+++ src/main/java/com/companyname/bank/App.java
@@ -0,0 +1,16 @@
+package com.companyname.bank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+	public String concatenate(String one, String two){
+        return one + two;
+    }
+}
